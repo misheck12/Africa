3d9feca4a2cdd43489f1fad4155462a22ca00ee3
misheck12 <misheck1720@gmail.com>
2022-07-15 23:34:19 +0200

    update comments validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,6 @@ class Comment < ApplicationRecord
     after_save :update_comment_counter
 
     def update_comment_counter
-        post.increment(:comment_counter)
+        post.increment(:comments_counter)
     end
 end
