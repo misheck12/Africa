040ae78e1ba493a4abf7fe15cb537746e34db918
misheck12 <misheck1720@gmail.com>
2022-07-15 23:21:44 +0200

    update post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,15 @@
 class Post < ApplicationRecord
-    has_many :likes
-    has_many :comments
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
-    after_save :update_post_counter
-  
-    def update_post_counter
-      author.increment(:posts_counter)
-    end
-  
-    def five_recent_comment(post)
-      Comment.where(post:).order(created_at: :desc).limit(5)
-    end
+  has_many :likes
+  has_many :comments
+  belongs_to :author, class_name: 'User', foreign_key: :author_id 
+
+  after_save :update_post_counter
+
+  def update_post_counter
+    author.increment(:posts_counter)
   end
+
+  def five_recent_comment
+    comments.order(created_at: :desc).limit(5)
+  end
+end
