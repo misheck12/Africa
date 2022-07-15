8c4a86a6a1ef2053d26771844e4f14ac0c3921e9
misheck12 <misheck1720@gmail.com>
2022-07-15 18:25:44 +0200

    create User relation class model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
-end
+    has_many :comments
+    has_many :likes
+    has_many :posts
+  
+    def three_recent_posts(author)
+      Post.where(author:).order(created_at: :desc).limit(3)
+    end
+  end
