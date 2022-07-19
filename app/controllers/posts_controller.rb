1b974e776b54139722821a4ba42caeae482c7133
misheck12 <misheck1720@gmail.com>
2022-07-19 20:10:31 +0200

    create form files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,33 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    puts('life in new')
+    @user = ApplicationController.new.current_user
+    @post = @user.posts.new
+    render :new, locals: { post: @post }
+  end
+
+  def create
+    @user = ApplicationController.new.current_user
+    add_post = @user.posts.new(post_params)
+    respond_to do |format|
+      format.html do
+        if add_post.save
+          flash[:success] = 'Post created successfully'
+          redirect_to user_posts_path
+        else
+          flash.now[:error] = 'Error: Post could not be created'
+          render :new, locals: { post: add_post }
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+      resources :likes, only: %i[new create]
+    end
   end
 
 
