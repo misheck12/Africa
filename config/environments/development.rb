cb7d8d6428fb6ba3285180c048e20e116593dcf9
misheck12 <misheck1720@gmail.com>
2022-07-25 20:38:40 +0200

    create N+1 configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,14 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.alert         = true
+    Bullet.bullet_logger = true
+    Bullet.console       = true
+    Bullet.rails_logger  = true
+    Bullet.add_footer    = true
+  end
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
