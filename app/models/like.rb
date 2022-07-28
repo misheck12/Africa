2c8115c73165150339d87c875a987e0dcd8a44b8
Ismail Courr <contact@ismailcourr.ml>
2022-07-28 21:48:01 +0100

    Fix like

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,6 +4,6 @@ class Like < ApplicationRecord
   after_save :update_like_counter
 
   def update_like_counter
-    post.increment(:likes_counter)
+    post.update(likes_counter: post.likes.count)
   end
 end
