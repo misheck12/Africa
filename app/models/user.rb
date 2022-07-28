e358c3a41ae688484db0bd0f2cb496080fdc2c38
misheck12 <misheck1720@gmail.com>
2022-07-28 21:13:36 +0200

    generate devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :name, length: { minimum: 1 }
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
