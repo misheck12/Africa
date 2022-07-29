6c1b9af28b780c9c28985f953e97b576b4bf0bc2
misheck12 <misheck1720@gmail.com>
2022-07-30 00:44:44 +0200

    fix errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,25 +1,14 @@
 class CommentsController < ApplicationController
-<<<<<<< HEAD
   before_action :authenticate_user!, only: %i[create destroy]
   load_and_authorize_resource
 
   def new
     @current_user = current_user
     @post = Post.find(params[:post_id])
-=======
-  def index
-    @comments = Comment.all
-  end
-
-  def show; end
-
-  def new
->>>>>>> 9fcc4eacc1865e4fd24c87cda75c6a5f5125329f
     @comment = Comment.new
   end
 
   def create
-<<<<<<< HEAD
     @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params)
     @comment.author_id = current_user.id
@@ -29,18 +18,6 @@ class CommentsController < ApplicationController
       redirect_to user_post_path(@post.author_id, @post.id)
     else
       render :new, alert: 'An error has occurred while creating the comment'
-=======
-    @comment = current_user.comments.new(comment_params)
-    @comment.post_id = params[:post_id]
-    respond_to do |format|
-      if @comment.save
-        format.html do
-          redirect_to user_post_path(current_user.id, params[:post_id]), notice: 'Post was successfully created.'
-        end
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
->>>>>>> 9fcc4eacc1865e4fd24c87cda75c6a5f5125329f
     end
   end
 
@@ -55,6 +32,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.permit(:text)
+    params.require(:comment).permit(:text)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,18 +3,12 @@ class PostsController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @users = User.all
     @user = User.find(params[:user_id])
-<<<<<<< HEAD
     @post = Post.where(author_id: @user.id).order(created_at: :desc)
-=======
-    @posts = @user.posts.includes(:comments, :author)
->>>>>>> 9fcc4eacc1865e4fd24c87cda75c6a5f5125329f
   end
 
   def show
     @user = User.find(params[:user_id])
-<<<<<<< HEAD
     @post = Post.includes(:author, :comments).find(params[:id])
   end
 
@@ -33,26 +27,6 @@ class PostsController < ApplicationController
       redirect_to @current_user
     else
       render :new, alert: 'An error has occurred while creating the post'
-=======
-    @post = @user.posts.find(params[:id])
-  end
-
-  def new
-    @post = current_user.posts.new(author_id: params[:user_id], comments_counter: 0, likes_counter: 0)
-  end
-
-  def create
-    @post = current_user.posts.new(post_params)
-    @post.comments_counter = 0
-    @post.likes_counter = 0
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to user_posts_path, notice: 'Post was successfully created.' }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
->>>>>>> 9fcc4eacc1865e4fd24c87cda75c6a5f5125329f
     end
   end
 
@@ -66,6 +40,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:author_id, :title, :text, :comments_counter, :likes_counter)
+    params.require(:post).permit(:title, :text)
   end
 end
