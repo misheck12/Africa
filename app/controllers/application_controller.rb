1963aaad94db27d936b8cc7da46db343326cb630
Ismail Courr <contact@ismailcourr.ml>
2022-07-29 18:41:01 +0100

    api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,39 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  before_action :authenticate_user!
-  before_action :update_allowed_parameters, if: :devise_controller?
+  protect_from_forgery with: :null_session
+  respond_to :json
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user
+  skip_before_action :verify_authenticity_token
 
   protected
 
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  def configure_permitted_parameters
+    attributes = %i[name email password]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
+
+  def authenticate_user
+    return unless request.headers['Authorization'].present?
+
+    begin
+      jwt_payload = JWT.decode(request.headers['Authorization'].split[1].remove('"'),
+                               Rails.application.secrets.secret_key_base).first
+      @current_user_id = jwt_payload['id']
+    rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+      head :unauthorized
+    end
+  end
+
+  def authenticate_user!(_options = {})
+    head :unauthorized unless signed_in?
+  end
+
+  def signed_in?
+    @current_user_id.present?
+  end
+
+  def current_user
+    @current_user ||= super || User.find(@current_user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope :api, defaults: { format: :json } do
+    devise_for :users, controllers: { sessions: :sessions, registrations: :registrations}
+  end
 
-  # Defines the root path route ("/")
-  root 'users#index'
-
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
-      resources :comments, only: %i[new create]
-      resources :likes, only: %i[new create]
+  root "users#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:new, :create, :destroy]
+      resources :likes, only: [:new, :create]
     end
   end
 
+  get '/api/users/:user_id/posts', to: 'api#user_posts', as: 'api_user_posts'
+  get '/api/users/:user_id/posts/:post_id/comments', to: 'api#user_post_comments', as: 'api_posts_comments'
+  post '/api/users/:user_id/posts/:post_id/comments', to: 'api#new_comment', as: 'new_post_comment'
 
 end
